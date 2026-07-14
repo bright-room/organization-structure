15326bdefc861a39938857b0ae4ef4f79eeda413
kukv <koki-nonaka@outlook.jp>
2026-07-14 18:25:39 +0900

    chore: remove unused organization secrets from locals

--- terraform/_locals.tf
+++ terraform/_locals.tf
@@ -2,10 +2,6 @@ locals {
   # Organization secret names (managed by organization-structure-administrator)
   organization_secrets = {
     chloe_chan_app_private_key = "CHLOE_CHAN_APP_PRIVATE_KEY"
-    pgp_signing_key            = "PGP_SIGNING_KEY"
-    pgp_signing_key_passphrase = "PGP_SIGNING_KEY_PASSPHRASE"
-    sonatype_central_password  = "SONATYPE_CENTRAL_PASSWORD"
-    sonatype_central_username  = "SONATYPE_CENTRAL_USERNAME"
   }
 
   # Organization variable names (managed by organization-structure-administrator)
